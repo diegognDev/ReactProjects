65e23fa3004b8fffd08bcf57f6323facb3ee0230
Diego <diegogarcianavas.4b@gmail.com>
2024-02-15 12:53:36 +0100

    get fact api call extracted from main component

---
+++ projects/react-prueba-tecnica/src/services/facts.js
@@ -0,0 +1,12 @@
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+export const getRandomFact = async () => {
+  try {
+    const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+    const data = await res.json()
+    const { fact } = data
+    return fact
+  } catch (err) {
+    return console.log(err)
+  }
+}
