5cc88e3c326d3d20ae44cac9f2a69dbf5d96b505
Diego <diegogarcianavas.4b@gmail.com>
2024-02-15 12:53:53 +0100

    added button to get new fact

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from 'react'
 import './app.css'
+import { getRandomFact } from './services/facts'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 //const CAT_ENDPOINT_RANDOM_IMAGE = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 export function App() {
   const [fact, setFact] = useState()
   const [image, setImage] = useState()
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res) => res.json())
-      .then((data) => {
-        const { fact } = data
-        setFact(fact)
-
-        const firstWord = fact.split(' ')[0] //Esto coge la primera palabra del array
-        //fact.split(' ')[0].slice(0,3).join(" ") Esto coge las 3 primeras palabras del array
-        setImage(
-          `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red`
-        )
-      })
-      .catch((err) => console.log(err))
+    getRandomFact().then(setFact)
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ')[0] //Esto coge la primera palabra del array
+    //fact.split(' ')[0].slice(0,3).join(" ") Esto coge las 3 primeras palabras del array
+    setImage(
+      `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red`
+    )
+  }, [fact])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
   return (
     <main>
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p>{fact}</p>}
       {image && (
         <img
