1db7c584dbf2d7a86127c70dd3d14662efebb3ae
Diego <diegogarcianavas.4b@gmail.com>
2024-02-15 14:14:14 +0100

    use of custom hooks

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,27 +1,14 @@
-import { useEffect, useState } from 'react'
 import './app.css'
-import { getRandomFact } from './services/facts'
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
 
 //const CAT_ENDPOINT_RANDOM_IMAGE = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 export function App() {
-  const [fact, setFact] = useState()
-  const [image, setImage] = useState()
-  useEffect(() => {
-    getRandomFact().then(setFact)
-  }, [])
-
-  useEffect(() => {
-    if (!fact) return
-    const firstWord = fact.split(' ')[0] //Esto coge la primera palabra del array
-    //fact.split(' ')[0].slice(0,3).join(" ") Esto coge las 3 primeras palabras del array
-    setImage(
-      `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red`
-    )
-  }, [fact])
+  const { fact, refreshRandomFact } = useCatFact()
+  const { image } = useCatImage({ fact })
 
   const handleClick = async () => {
-    const newFact = await getRandomFact()
-    setFact(newFact)
+    refreshRandomFact()
   }
   return (
     <main>
