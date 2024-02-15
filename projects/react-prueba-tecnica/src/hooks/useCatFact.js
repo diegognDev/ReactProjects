bbcfd0d6521cdf4e0fa2474a547697503508a7cd
Diego <diegogarcianavas.4b@gmail.com>
2024-02-15 14:13:56 +0100

    fetch random fact using custom hook

---
+++ projects/react-prueba-tecnica/src/hooks/useCatFact.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+import { getRandomFact } from '../services/facts'
+
+export function useCatFact() {
+  const [fact, setFact] = useState()
+  const refreshRandomFact = () => {
+    getRandomFact().then(setFact)
+  }
+  useEffect(refreshRandomFact, [])
+
+  return { fact, refreshRandomFact }
+}
