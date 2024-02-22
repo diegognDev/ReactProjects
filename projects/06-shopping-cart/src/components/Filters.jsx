86494093b1ca08a31916841eef0a3f5d081d5284
Diego <diegogarcianavas.4b@gmail.com>
2024-02-22 20:34:07 +0100

    use of useId hook for inputs id

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 export function Filters({ changeFilters }) {
   const [minPrice, setMinPrice] = useState(0)
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
 
   const handleChange = (event) => {
     setMinPrice(event.target.value)
@@ -22,7 +24,7 @@ export function Filters({ changeFilters }) {
         <label htmlFor="price">Precio minimo: </label>
         <input
           type="range"
-          id="price"
+          id={minPriceFilterId}
           min="0"
           max="1000"
           onChange={handleChange}
@@ -30,8 +32,8 @@ export function Filters({ changeFilters }) {
         <span>{minPrice} </span>
       </div>
       <div>
-        <label htmlFor="category">Categoria</label>
-        <select id="category" onChange={handleCategoryChange}>
+        <label htmlFor={categoryFilterId}>Categoria</label>
+        <select id={categoryFilterId} onChange={handleCategoryChange}>
           <option value="all">All</option>
           <option value="laptops">Portatiles</option>
           <option value="smartphones">Moviles</option>
