0d08c611ad5ceeee5aec7649195831f9e9a837f8
Diego <diegogarcianavas.4b@gmail.com>
2024-03-06 12:04:08 +0100

    use os provider

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,16 +3,19 @@ import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { useFilters } from './hooks/useFilters'
 import { IS_DEVELOPMENT } from './config'
+import { Cart } from './components/Cart'
+import { CartProvider } from './context/cart'
 
 function App() {
-  const { filters, products, filterProducts } = useFilters()
+  const { products, filterProducts } = useFilters()
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filterProducts(products)} />
-      {IS_DEVELOPMENT && <Footer filters={filters} />}
-    </>
+      {IS_DEVELOPMENT && <Footer />}
+    </CartProvider>
   )
 }
 
